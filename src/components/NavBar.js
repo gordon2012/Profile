82790fe94c63abaa2e71a254ce798e50d3142bec
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-03-22 11:41:39 -0700

    Create basic structure (Closes #1)

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div className="NavBar">
+          <img src="https://avatars1.githubusercontent.com/u/9071982?v=3&u=d62e11320f5831d572a0f62a7ad68a5acc980cee&s=400" alt=""/>
+          <div><span>Gordon Doskas</span></div>
+      </div>
+    );
+  }
+}
